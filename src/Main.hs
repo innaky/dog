6d6fa2aec7bd52ffdd1466ec98c2d6072fd41826
Innaky <innaky@protonmail.com>
2020-03-19 21:17:21 -0400

    Fusion files in column form Check this functions and edit for match with [String] -> IO

--- src/Main.hs
+++ src/Main.hs
@@ -4,10 +4,12 @@ import System.Environment
 import System.Directory
 import System.IO
 import Data.List
+import System.Directory.Tree (FileName)
 
 key :: [(String, [String] -> IO ())]
 key = [("add", add)
-      , ("remove", remove)]
+      , ("remove", remove)
+      , ("cat", catFile)]
 
 add :: [String] -> IO ()
 add [fileName, input] = appendFile fileName (input ++ "\n")
@@ -25,6 +27,29 @@ remove [fileName, numberString] = do
   hClose tempHandle
   removeFile fileName
   renameFile tempName fileName
+
+twoHandles :: Handle -> Handle -> IO String
+twoHandles firstH secondH = do
+  end <-hIsEOF firstH
+  if end
+    then return $ "EOF"
+    else do fLine <- hGetLine firstH
+            sLine <- hGetLine secondH
+            let fusionLines = fLine ++ " " ++ sLine
+            return $ fusionLines
+            twoHandles firstH secondH
+            
+catFile :: FilePath -> FilePath -> FileName -> IO ()
+catFile firstFile secondFile outFile = do
+  firstHandle <- openFile firstFile ReadMode
+  secondHandle <- openFile secondFile ReadMode
+  (tempName, tempHandle) <- openTempFile "." "temp"
+  fusionLines <- twoHandles firstHandle secondHandle
+  hPutStrLn tempHandle fusionLines
+  hClose firstHandle
+  hClose secondHandle
+  hClose tempHandle
+  renameFile tempName outFile
 
 main :: IO ()
 main = do
